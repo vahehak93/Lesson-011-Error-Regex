bf94425323f43dd07fe3c1bca1f30115259dac88
vahehak93 <vahehak@gmail.com>
2021-12-21 02:33:43 +0400

    Initial commit

---
+++ 5.regex_tutorials.js
@@ -0,0 +1,70 @@
+/*Pass following tutorial and convert all the codes into js with usage.*/
+
+// let str = "abcdefg";
+// let regexp = /abc/;
+// console.log(regexp.test(str));
+
+
+// let str1 = "abc123xyz";
+// let regexp1 = /123/;
+// console.log(regexp1.test(str1));
+
+
+// let str = "asda.s1215";
+// let regexp = /...\./;
+// console.log(regexp.test(str));
+
+
+// let str = "man";
+// let regexp = /[^drp]an/;
+// console.log(regexp.test(str));
+
+
+// let str = "dogs";
+// let regexp = /[hd]og/;
+// console.log(regexp.test(str));
+
+
+// let str = "Bob";
+// let regexp = /[A-C][n-p][a-c]/;
+// console.log(regexp.test(str));
+
+
+// let str = "wazzzzzup";
+// let regexp = /waz{3,5}up/;
+// console.log(regexp.test(str));
+
+
+// let str = "aacc";
+// let regexp = /aa+b*c+/;
+// console.log(regexp.test(str));
+
+
+// let str = "	1.   abc";
+// let regexp = /\d\.\s+abc/;
+// console.log(regexp.test(str));
+
+
+// let str = "Mission:successful";
+// let regexp = /^Mission:successful$/;
+// console.log(regexp.test(str));
+
+
+// let str = "file_record_transcript.pdf";
+// let regexp = /^(file.+)\.pdf$/;
+// console.log(regexp.test(str));
+
+
+// let str = "Aug 2011";
+// let regexp = /(\w+ (\d+))/;
+// console.log(regexp.test(str));
+
+
+// let str = "1024x768";
+// let regexp = /(\d+)x(\d+)/;
+// console.log(regexp.test(str));
+
+
+// let str = "	I love dogs";
+// let regexp = /I love (cats|dogs)/;
+// console.log(regexp.test(str));
